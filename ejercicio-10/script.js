e2ce362a4a2b9175f99b3690d102d7dafe3f7f05
LucaBonansea <liberatorsouls85@gmail.com>
2026-05-20 17:20:18 -0300

    Agregar archivos index.html, script.js y styles.css; implementar funcionalidad de botones y estilos

---
+++ ejercicio-10/script.js
@@ -0,0 +1,99 @@
+const $btn1 = document.querySelector(".btn-1");
+
+$btn1.addEventListener("click", function(){
+    document.querySelector(".text-1").textContent = "Primer texto cambiado";
+});
+
+
+
+const $btn2 = document.querySelector(".btn-2");
+
+
+$btn2.addEventListener("click", function(){
+    document.querySelector(".text-2").textContent = "Segundo texto cambiado!";
+});
+
+
+let $img = document.querySelector(".imagen");
+
+$img.src = "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/13/cb/95/57/img-20180721-wa0042-largejpg.jpg?w=900&h=500&s=1";
+
+
+
+
+const $send = document.querySelector(".send");
+
+$send.addEventListener("click", function(){
+    let $input = document.querySelector(".input-user");
+    let $input2 = document.querySelector(".contra");
+
+    console.log($input.value);
+    console.log($input2.value);
+});
+
+const $decoration = document.querySelector(".decoration");
+
+$decoration.classList.add("red");
+
+const $descorar = document.querySelector(".descorar");
+
+$descorar.classList.remove("descorar");
+
+const $oscuro = document.querySelector(".oscuro");
+
+$oscuro.addEventListener("click", function(){
+    document.body.classList.toggle("dark");
+});
+
+
+
+const $lista = document.querySelector(".lista");
+
+const $agregar = document.querySelector(".add");
+
+
+$agregar.addEventListener('click', function(){
+    
+    const $nuevopunto = document.createElement("li");
+    $nuevopunto.textContent = "Nuevo punto";
+
+    $lista.appendChild($nuevopunto);
+});
+
+/*----------------------*/
+const $nombre = document.querySelector(".nombre-input");
+const $color = document.querySelector(".color-input");
+const $mensaje = document.querySelector(".mensaje-input");
+
+const $enviar = document.querySelector(".enviar");
+
+
+/*----------------------*/
+const $output_nombre = document.querySelector(".nombre");
+const $output_color = document.querySelector(".color");
+const $output_mensaje = document.querySelector(".mensaje");
+
+
+/*----------------------*/
+const $result = document.querySelector(".resultado");
+
+
+$enviar.addEventListener('click', function(){
+    $output_nombre.textContent = $nombre.value;
+    $output_color.textContent = $color.value;
+    $output_mensaje.textContent = $mensaje.value;
+    $result.classList.add("card");
+
+    const $card = document.createElement("div");
+    $card.innerHTML += "<h2> Nombre: " + $nombre.value + "</h2>";
+    $card.innerHTML += "<h2> Color favorito: " + $color.value + "</h2>";
+    $card.innerHTML += "<h2> Mensaje:" + $mensaje.value + "</h2>";
+
+    if($nombre.value != "" && $color.value != "" && $mensaje.value != ""){
+        document.body.append($card);
+    }else{
+        alert("Por favor, completa todos los campos.");
+    }
+ 
+});
+
